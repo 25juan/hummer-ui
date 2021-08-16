f4e8f9a5a9b18b0aab8768f72de0483784f9a34f
kongdong <1216181067@qq.com>
2021-08-16 19:04:27 +0800

    添加demo库

---
+++ example/entry.js
@@ -0,0 +1,5 @@
+import React from "react"
+import * as Tenon from '@hummer/tenon-react'
+import App from "./src/App";
+
+Tenon.render(<App />)
---
+++ example/src/App.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default () => {
+  return <text>sss</text>
+}
